c57ef2e2c3941a23a296aef862d0be99c6b9e8f4
jimmy <jhtokd@gmail.com>
2019-12-14 16:52:06 +0800

    feat: add axios, jwt, store jwt into localstorage

---
+++ public/javascripts/login.js
@@ -0,0 +1,21 @@
+document.querySelector('#submit').addEventListener('click', () => Submit());
+
+function Submit(){
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    let data = {
+        email,
+        password
+    };
+
+    axios.post('../login',data).then(res=>{
+        // console.log('recieve res:  ', res);
+        if(res.data.success){
+            const token = res.data.token;
+            localStorage.setItem('token', token);
+            window.location.href = "../";
+        }else{
+            alert('帳號或密碼錯誤');
+        }
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,74 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 router.get('/', function(req, res, next) {
-  var db = req.con;
-  db.query('SELECT * FROM account', function(err, rows) {
-      if (err) {
-          console.log(err);
-      }
-      var data = rows;
-      res.render('index', { title: 'Account Information', data: data});
-  });
+	var db = req.con;
+	db.query('SELECT * FROM account', function(err, rows) {
+		if (err) {
+			console.log(err);
+		}
+		var data = rows;
+		res.render('index', { title: 'Account Information', data: data});
+	});
 });
 
 // register
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Add User'});
+res.render('register', { title: 'Add User'});
 });
 router.post('/api/register/user', function(req, res, next) {
-  var db = req.con;
-  var sql = {
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password,10)
-  };
-  db.query('INSERT INTO account SET ?', sql, function(err, rows) {
-      if (err) {
-          console.log(err);
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.redirect('/');
-  });
+	var db = req.con;
+	var sql = {
+		email: req.body.email,
+		password: bcrypt.hashSync(req.body.password,10)
+	};
+	db.query('INSERT INTO account SET ?', sql, function(err, rows) {
+		if (err) {
+			console.log(err);
+		}
+		res.setHeader('Content-Type', 'application/json');
+		res.redirect('/');
+	});
 });
 
 // login
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'login'});
+	res.render('login', { title: 'login'});
 });
-
 router.post('/login', function(req, res, next) {
-  var db = req.con;
-  let {email, password} = req.body;
-  const routerRes = res;
-
-  db.query("select * from account where email = ?", email, function(err, rows) {
-    if (err) {
-        console.log('err:  ',err);
-    }
-    var data = rows[0];
-
-    bcrypt.compare(password, data.password).then(function (res) {
-      console.log('res:   ', res); // true
-      if(res){
-        routerRes.redirect('/');
-      }
-    });
-  });
+	var db = req.con;
+	let {email, password} = req.body;
+	const routerRes = res;
+	db.query("select * from account where email = ?", email, function(err, rows) {
+		if (err) {
+			console.log('err:  ',err);
+		}
+		var data = rows[0];
+		if(data){
+			bcrypt.compare(password, data.password).then(function (res) {
+				console.log('res:   ', res); // true
+				if(res){
+					// 產生 JWT
+					const payload = {
+						user_id: data.id,
+					};
+					const token = jwt.sign({ payload, exp: Math.floor(Date.now() / 1000) + (60 * 15) }, 'my_secret_key');
+					console.log('token:   ', token);
+					// routerRes.redirect('/');
+					routerRes.json({
+						success: true,
+						token
+					})
+				}else{
+					routerRes.json({ success: false });
+				}
+			});
+		}else{
+			routerRes.json({ success: false });
+		}
+	});
 });
 
 module.exports = router;
