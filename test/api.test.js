33af5d05e9459f10da80153bc807608bce3f27f8
jimmy <jhtokd@gmail.com>
2019-12-20 17:56:23 +0800

    feat: add test case into api/login

--- test/api.test.js
+++ test/api.test.js
@@ -6,6 +6,7 @@ const aclAuth = require('../auths/aclAuth');
 let jwtStub = null;
 let aclStub = null;
 let app = null;
+let api = null;
 
 beforeEach(function() {
 	jwtStub = sinon.stub(jwtAuth, 'auth')
@@ -23,6 +24,7 @@ beforeEach(function() {
 	app.use(express.urlencoded({ extended: false }));
 	const apiRouter = require('../routes/api');
 	app.use('/api', apiRouter);
+	api = supertest(app);
 });
 
 afterEach(() => {
@@ -37,7 +39,6 @@ afterAll(done => {
 
 describe('api/login', () => {
     it('it should return a 200 response', async done => {
-        let api = supertest(app);
         api.post('/api/login')
             .send({name: 'jimmy', password: '0000'})
             .expect(200)
@@ -46,16 +47,34 @@ describe('api/login', () => {
 				expect(err).toBe(null);
                 done();
             });
-    }); 
+	}); 
+	it('wrong password should return success: false', async done => {
+        api.post('/api/login')
+            .send({name: 'jimmy', password: '0011'})
+            .expect(200)
+            .end((err, res) => {
+				expect(res.body.success).toBe(false);
+				expect(err).toBe(null);
+                done();
+            });
+	}); 
+	it('user not found should return success: false', async done => {
+        api.post('/api/login')
+            .send({name: 'jyyy', password: '0011'})
+            .expect(200)
+            .end((err, res) => {
+				expect(res.body.success).toBe(false);
+				expect(err).toBe(null);
+                done();
+            });
+	}); 
 });
 
 describe('api/users', () => {
     it('GET should return users', async done => {
-        let api = supertest(app);
         api.get('/api/users')
             .expect(200)
             .end((err, res) => {
-				console.log('------res: ', res.body);
 				expect(res.body.success).toBe(true);
 				const {users} = res.body;
 
